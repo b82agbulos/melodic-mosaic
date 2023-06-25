6ef5e7393a54ec71881410e11a2fcfac5a7b9a2a
Bobby Agbulos <124684273+b82agbulos@users.noreply.github.com>
2023-06-25 10:33:56 -0500

    Add files via upload
    completed onslaught-amplified

--- onslaught-stats.js
+++ onslaught-stats.js
@@ -22,14 +22,14 @@ function calculateStats() {
 
   const sortedAlbumCounts = Object.entries(artistAlbumCounts)
     .sort((a, b) => b[1] - a[1])
-    .filter(([artist, count]) => count > 1)
+    .filter(([artist, count]) => count > 2)
     .slice(0, 50);
   const sortedRatingPoints = Object.entries(artistRatingPoints)
     .filter(([artist, points]) => points > 2)  
     .sort((a, b) => b[1] - a[1])
     .slice(0, 50);
   const sortedGenreCounts = Object.entries(genreCounts)
-  .filter(([genre, count]) => count > 1)
+  .filter(([genre, count]) => count > 2)
     .sort((a, b) => b[1] - a[1])
     .slice(0, 50);
 
